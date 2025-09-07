8d9a90ed1688717d80b9403cb46ed2c4b6481f48
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2025-09-07 12:53:15 +0530

    Import all libary in main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 #main.py
+import tkinter as tk
+from tkinter import ttk, filedialog, messagebox
+import cv2
+import numpy as np
+import threading
+import os
+from PIL import Image, ImageTk
+import json
+from datetime import datetime
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+
